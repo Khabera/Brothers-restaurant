ef541380c34af59178143ac3af4fbd1b90befb6b
Shenmew <Cessnator007@gmail.com>
2021-11-28 15:56:30 -0800

    Reformat

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,6 @@
 import './footer.css'
 let footer = document.createElement('div');
 footer.setAttribute('id', 'footer');
-footer.textContent = `Coded by Khabera using Vanilla JS with a focus on ES6 Modules in Webpack`
+footer.textContent = `Webpage developed by Khabera using Vanilla JS with a focus on ES6 Modules in Webpack`
 
 export default footer;
